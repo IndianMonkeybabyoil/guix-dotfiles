a7ef0ceceda6c44f64dc27fdcf62fb938339e804
IndianMonkeybabyoil <muneetpaneer65@gmail.com>
2026-06-18 09:28:01 -0400

    Add files via upload
    scubaaaaaaaa

---
+++ init.sh
@@ -0,0 +1,104 @@
+#!/bin/zsh
+
+DRIVE=$1
+
+sfdisk "$DRIVE" <<EOF
+label: gpt
+size=1G, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+EOF
+
+mkfs.ext4 -L guix-root "${DRIVE}2"
+mkfs.fat -F32 -n EFI "${DRIVE}1"
+
+mount /dev/disk/by-label/guix-root /mnt
+
+mkdir -p /mnt/boot/efi
+mount /dev/disk/by-label/EFI /mnt/boot/efi
+
+herd start cow-store /mnt
+
+mkdir -p ~/.config/guix
+
+cat << EOF > ~/.config/guix/channels.scm
+(cons* (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB"))))
+       %default-channels)
+EOF
+
+guix pull
+
+mkdir -p /mnt/etc
+
+cat << EOF > /mnt/etc/config.scm
+(define-module (system-config)
+  #:use-module (gnu)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu packages nvidia)
+  #:use-module (nongnu system linux-initrd)
+  #:use-module (nonguix transformations)
+  #:use-module (gnu services wm)
+  #:use-module (gnu services desktop)
+  #:use-module (gnu services xorg)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages version-control))
+
+(use-service-modules desktop networking ssh xorg)
+
+(define %my-os
+  (operating-system
+    (kernel linux)
+    (initrd microcode-initrd)
+    (firmware (list linux-firmware amdgpu-firmware))
+    (locale "en_US.utf8")
+    (timezone "America/New_York")
+    (keyboard-layout (keyboard-layout "us"))
+
+    (bootloader (bootloader-configuration
+                 (bootloader grub-efi-bootloader)
+                 (targets '("/boot/efi"))
+                 (keyboard-layout keyboard-layout)))
+
+    (kernel-arguments
+     (append '("modprobe.blacklist=nouveau")
+             %default-kernel-arguments))
+
+    (file-systems (cons* (file-system
+                           (mount-point "/")
+                           (device (file-system-label "guix-root"))
+                           (type "ext4"))
+                         (file-system
+                           (mount-point "/boot/efi")
+                           (device (file-system-label "EFI"))
+                           (type "vfat"))
+                         %base-file-systems))
+
+    (users (cons (user-account
+                  (name "goomba")
+                  (comment "Main User")
+                  (group "users")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video" "input")))
+                %base-user-accounts))
+
+    (packages (append (list hyprland kitty rofi-wayland git zsh mesa waybar) %base-packages))
+
+    (services
+     (append (list
+              (service nvidia-service-type)
+              (service hyprland-service-type))
+             %desktop-services))))
+((nonguix-transformation-nvidia #:driver nvda) %my-os)
+EOF
+
+guix system init /mnt/etc/config.scm /mnt
+reboot
---
+++ setup.sh
@@ -0,0 +1,141 @@
+#!/bin/zsh
+
+set -e
+
+cat << EOF > ~/.zprofile
+if [ -f ~/.guix-profile/etc/profile ]; then
+  . ~/.guix-profile/etc/profile
+fi
+
+if [ -f ~/.config/guix/current/etc/profile ]; then
+  . ~/.config/guix/current/etc/profile
+fi
+EOF
+
+cat << EOF > ~/home-configuration.scm
+(define-module (home-config)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages text-editors)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages version-control))
+
+(home-environment
+ (packages
+  (list neovim
+        htop
+        rofi-wayland
+        git
+        btop))
+
+ (services
+  (list
+   (service home-zsh-service-type
+            (home-zsh-configuration
+             (zshrc (list (plain-file "zshrc" "
+export ZSH=\$HOME/.oh-my-zsh
+ZSH_THEME=\"robbyrussell\"
+plugins=(git zsh-autosuggestions)
+
+if [ -f \$ZSH/oh-my-zsh.sh ]; then
+  source \$ZSH/oh-my-zsh.sh
+fi
+
+if [ -f ~/.zprofile ]; then
+  source ~/.zprofile
+fi
+")))))
+
+   (service home-environment-variables-service-type
+            '(("XDG_SESSION_TYPE" . "wayland")
+              ("ELECTRON_OZONE_PLATFORM_HINT" . "auto"))))))
+EOF
+
+guix home reconfigure ~/home-configuration.scm
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+mkdir -p ~/.config/kitty
+
+cat << EOF > ~/.config/kitty/kitty.conf
+font_family      JetBrains Mono
+font_size        11.0
+background_opacity 0.85
+confirm_os_window_close 0
+enable_audio_bell no
+EOF
+
+mkdir -p ~/.config/hypr
+
+cat << EOF > ~/.config/hypr/hyprland.conf
+exec-once = lspci | grep -qi nvidia && hyprctl setenv LIBVA_DRIVER_NAME nvidia
+exec-once = lspci | grep -qi nvidia && hyprctl setenv GBM_BACKEND nvidia-drm
+exec-once = lspci | grep -qi nvidia && hyprctl setenv __GLX_VENDOR_LIBRARY_NAME nvidia
+exec-once = waybar
+
+env = XDG_SESSION_TYPE,wayland
+env = ELECTRON_OZONE_PLATFORM_HINT,auto
+
+cursor {
+    no_hardware_cursors = true
+}
+
+bind = SUPER, Return, exec, kitty
+bind = SUPER, D, exec, rofi -show drun
+
+monitor=,preferred,auto,1
+
+input {
+    kb_layout = us
+    follow_mouse = 1
+
+    touchpad {
+        natural_scroll = true
+        tap-to-click = true
+    }
+}
+
+general {
+    gaps_in = 5
+    gaps_out = 10
+    border_size = 2
+    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+    col.inactive_border = rgba(595959aa)
+    layout = dwindle
+}
+
+decoration {
+    rounding = 10
+    blur {
+        enabled = true
+        size = 3
+        passes = 1
+    }
+}
+
+bind = SUPER, C, killactive
+bind = SUPER, M, exit
+bind = SUPER, V, togglefloating
+bind = SUPER, F, fullscreen
+
+bind = SUPER, left, movefocus, l
+bind = SUPER, right, movefocus, r
+bind = SUPER, up, movefocus, u
+bind = SUPER, down, movefocus, d
+
+bind = SUPER, 1, workspace, 1
+bind = SUPER, 2, workspace, 2
+bind = SUPER, 3, workspace, 3
+bind = SUPER, 4, workspace, 4
+
+bind = SUPER SHIFT, 1, movetoworkspace, 1
+bind = SUPER SHIFT, 2, movetoworkspace, 2
+bind = SUPER SHIFT, 3, movetoworkspace, 3
+bind = SUPER SHIFT, 4, movetoworkspace, 4
+EOF
+
+source ~/.zprofile
+Hyprland
